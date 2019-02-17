9718bf354248592fd8afe8a2c1c665d6903526cb
熊东彦 <18804609093@163.com>
2019-02-17 17:43:20 +0800

    Update each_seq_GCcount.pl

--- Basic/each_seq_GCcount.pl
+++ Basic/each_seq_GCcount.pl
@@ -15,21 +15,21 @@ my $Muti_seq = Bio::SeqIO->new(
 	-file => $Muti_seq_file,
 	-format => 'fasta',
 );
-my @GC_count;
+my %GC_count;
 my $j = 1;
 while (my $seq = $Muti_seq->next_seq()) {
 	my $id = $seq -> id;
 	my $seq_seq = $seq -> seq;
 	my $seq_length = length$seq_seq;
 	my @split_seq = split//,$seq_seq;
-	my $GC_count;
+	my $GC_co;
 	for (my $i=0; $i<@split_seq; $i= $i+1,){
-		if(@split_seq[$i] eq "G"){$GC_count = $GC_count + 1}
-		elsif(@split_seq[$i] eq "C"){$GC_count = $GC_count + 1}
-		else{$GC_count = $GC_count}
+		if(@split_seq[$i] eq "G"){$GC_co = $GC_co + 1}
+		elsif(@split_seq[$i] eq "C"){$GC_co = $GC_co + 1}
+		else{$GC_co = $GC_co}
 	};
-	$GC_count = $GC_count/$seq_length;
-	@GC_count[$j] = $id . "\t" . "GC count= " . $GC_count . "\n";
+	$GC_co = $GC_co/$seq_length;
+	$GC_count{$id} = "\t"."GC_count: ".$GC_co."\n";
 	$j = $j + 1;
 };
-print @GC_count;
+print %GC_count;
