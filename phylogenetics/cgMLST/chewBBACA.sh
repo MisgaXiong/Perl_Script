5726bf668ecc3ee74cf7519634e4e5c7cd60994f
熊东彦 <18804609093@163.com>
2019-03-05 15:29:07 +0800

    Create chewBBACA.sh

---
+++ phylogenetics/cgMLST/chewBBACA.sh
@@ -0,0 +1,12 @@
+# refgenome/ Folder containing the genomes from which schema will be created. Alternatively a file containing the path to the list of reference genomes. 
+# ./genomes.fa/ Folder containing the query genomes means the genomes you interested in.
+# https://github.com/theInnuendoProject/chewBBACA
+
+chewBBACA.py CreateSchema -i ./refgenome/ -o Step1_refgenes --cpu 12
+cd Step1_refgenes && rm Bsuis* #leave only one reference genome
+cd short && rm Bsuis*
+cd ../
+cd ../
+chewBBACA.py AlleleCall -i ./genomes.fa/ -g Step1_refgenes/ -o Step2_allelecall_use_wgMLST --cpu 12
+chewBBACA.py TestGenomeQuality -i Step2_allelecall_use_wgMLST/results_20190303T161746/results_alleles.tsv -n 12 -t 200 -s 5 -o Step3_Evaluate_wgMLST_call_quality
+chewBBACA.py SchemaEvaluator -i Step1_refgenes/ -ta 11 -l rms/ratemyschema.html --cpu 12 --title "my title"
