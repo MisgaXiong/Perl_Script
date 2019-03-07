ebf6123e57d4e1062e7e292b2f68268998363a1e
熊东彦 <18804609093@163.com>
2019-03-07 09:52:06 +0800

    Update chewBBACA.sh

--- phylogenetics/cgMLST/chewBBACA.sh
+++ phylogenetics/cgMLST/chewBBACA.sh
@@ -9,4 +9,5 @@ cd ../
 cd ../
 chewBBACA.py AlleleCall -i ./genomes.fa/ -g Step1_refgenes/ -o Step2_allelecall_use_wgMLST --cpu 12
 chewBBACA.py TestGenomeQuality -i Step2_allelecall_use_wgMLST/results_20190303T161746/results_alleles.tsv -n 12 -t 200 -s 5 -o Step3_Evaluate_wgMLST_call_quality
+chewBBACA.py ExtractCgMLST -i Step2_allelecall_use_wgMLST/results_2019030T161756/results_alleles.tsv -o Step4_cgMLST_use_alleletsv
 chewBBACA.py SchemaEvaluator -i Step1_refgenes/ -ta 11 -l rms/ratemyschema.html --cpu 12 --title "my title"
